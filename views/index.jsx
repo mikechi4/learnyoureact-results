cec569e054c72302101fa2a36ac995a25f880c0e
Mike <mike.chi4@gmail.com>
2017-02-11 15:42:31 -0500

    finished state problem

--- views/index.jsx
+++ views/index.jsx
@@ -43,16 +43,26 @@ class TodoForm extends React.Component {
 }
 
 class Todo extends React.Component {
-  // Write code here
-  render(){
-    return(
+  constructor(props) {
+    super(props);
+    this.state = {checked: true};
+  }
+  handleChange(){
+    this.setState({checked: false})
+  }
+  render() {
+    return (
       <tr>
+        <td style={{border: "1px solid black"}}>
+          <input type="checkbox" onChange={this.handleChange.bind(this)}/>
+        </td>
         <td style={{border: "1px solid black"}}>{this.props.title}</td>
         <td style={{border: "1px solid black"}}>{this.props.children}</td>
       </tr>
-    )
+    );
   }
 }
+
 Todo.propTypes = {
     title: React.PropTypes.string.isRequired
 };
