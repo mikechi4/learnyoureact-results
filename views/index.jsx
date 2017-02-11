6418eb7ea0d67564951cd1478b187ff1a5f832e4
Mike <mike.chi4@gmail.com>
2017-02-11 15:44:38 -0500

    style section

--- views/index.jsx
+++ views/index.jsx
@@ -56,8 +56,8 @@ class Todo extends React.Component {
         <td style={{border: "1px solid black"}}>
           <input type="checkbox" onChange={this.handleChange.bind(this)}/>
         </td>
-        <td style={{border: "1px solid black"}}>{this.props.title}</td>
-        <td style={{border: "1px solid black"}}>{this.props.children}</td>
+        <td style={style.tableContent}>{this.props.title}</td>
+        <td style={style.tableContent}>{this.props.children}</td>
       </tr>
     );
   }
@@ -65,4 +65,10 @@ class Todo extends React.Component {
 
 Todo.propTypes = {
     title: React.PropTypes.string.isRequired
+};
+
+let style = {
+    tableContent: {
+        border: "1px solid black"
+    }
 };
